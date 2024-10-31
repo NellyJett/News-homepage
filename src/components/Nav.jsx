cd481485dda559e687efefd59c203e2c01df7625
Nelly-B <alapanelly@gmail.com>
2024-10-31 18:35:44 +0100

    reworked navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Menuopen, MenuClose, Logo } from '../icons';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -13,30 +13,35 @@ const Navbar = () => {
     <nav className="navbar w-full p-6">
       <div className="flex items-center justify-between p-4 bg-[#fff] text-black">
         <Logo className="h-8 w-auto" />
-
         <div className="menu-icon sm:hidden block" onClick={toggleMenu}>
           {isOpen ? <MenuClose fill="#000000" /> : <Menuopen fill="#000000" />}
         </div>
+      </div>
 
-        <div className={`menu ${isOpen ? 'block' : 'hidden'} sm:flex transition-all duration-300 ease-in-out`}>
-          <ul className="flex flex-col sm:flex-row">
-            <li className="p-2 hover:text-[#f6a833]">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="p-2 hover:text-[#f6a833]">
-              <Link to="/new">New</Link>
-            </li>
-            <li className="p-2 hover:text-[#f6a833]">
-              <Link to="/popular">Popular</Link>
-            </li>
-            <li className="p-2 hover:text-[#f6a833]">
-              <Link to="/trending">Trending</Link>
-            </li>
-            <li className="p-2 hover:text-[#f6a833]">
-              <Link to="/categories">Categories</Link>
-            </li>
-          </ul>
-        </div>
+      {/* Menu */}
+      <div
+        className={`menu ${
+          isOpen ? 'flex' : 'hidden'
+        } sm:flex transition-all duration-300 ease-in-out absolute top-16 left-0 h-screen bg-[#fff]`}
+        style={{ width: isOpen ? '50%' : '0' }} // Set width to 50% on small screens
+      >
+        <ul className="flex flex-col h-full justify-start">
+          <li className="p-2 hover:text-[#f6a833]">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="p-2 hover:text-[#f6a833]">
+            <Link to="/new">New</Link>
+          </li>
+          <li className="p-2 hover:text-[#f6a833]">
+            <Link to="/popular">Popular</Link>
+          </li>
+          <li className="p-2 hover:text-[#f6a833]">
+            <Link to="/trending">Trending</Link>
+          </li>
+          <li className="p-2 hover:text-[#f6a833]">
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
